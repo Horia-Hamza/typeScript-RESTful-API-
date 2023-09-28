08916187e555b701f2fb8abc8dd34d10749c4bbd
Horia-Hamza <horiahamza24@gmail.com>
2023-09-28 19:56:48 +0300

    Your commit message here

--- dist/src/middleWare/authentication.js
+++ dist/src/middleWare/authentication.js
@@ -1,4 +1,27 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -13,7 +36,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.auth = exports.roles = void 0;
-const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const jsonwebtoken_1 = __importStar(require("jsonwebtoken")); // Import TokenExpiredError for handling token expiration
 const user_1 = __importDefault(require("../../DB/models/user"));
 const errorHandling_js_1 = require("../services/errorHandling.js");
 exports.roles = {
@@ -24,37 +47,43 @@ exports.roles = {
 const auth = (accessRoles = [exports.roles.User]) => {
     return (0, errorHandling_js_1.asyncHandler)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
         const { authorization } = req.headers;
-        if (!(authorization === null || authorization === void 0 ? void 0 : authorization.startsWith(process.env.bearerKey))) {
-            const error = new Error("in-valid token or bearer key");
-            error.cause = 200;
-            return next(error);
-        }
-        else {
-            const token = authorization.split(process.env.bearerKey)[1];
-            const decoded = jsonwebtoken_1.default.verify(token, process.env.tokenSignature);
-            if (!(decoded === null || decoded === void 0 ? void 0 : decoded.id) || !(decoded === null || decoded === void 0 ? void 0 : decoded.isLoggedIn)) {
-                const error = new Error("in-valid token payload");
-                error.cause = 200;
-                return next(error);
+        try {
+            if (!(authorization === null || authorization === void 0 ? void 0 : authorization.startsWith(process.env.bearerKey))) {
+                throw new Error("Invalid token or bearer key");
             }
             else {
-                const user = yield user_1.default.findById(decoded.id);
-                if (!user) {
-                    const error = new Error("Not register user");
-                    error.cause = 401;
-                    return next(error);
+                const token = authorization.split(process.env.bearerKey)[1];
+                const decoded = jsonwebtoken_1.default.verify(token, process.env.tokenSignature);
+                if (!(decoded === null || decoded === void 0 ? void 0 : decoded.id) || !(decoded === null || decoded === void 0 ? void 0 : decoded.isLoggedIn)) {
+                    throw new Error("Invalid token payload");
                 }
                 else {
-                    if (!accessRoles.includes(user.role)) {
-                        const error = new Error("Un-authorized user");
-                        error.cause = 403;
-                        return next(error);
+                    const user = yield user_1.default.findById(decoded.id);
+                    if (!user) {
+                        throw new Error("Not registered user");
                     }
                     else {
-                        req.user = user;
-                        return next();
+                        if (!accessRoles.includes(user.role)) {
+                            throw new Error("Unauthorized user");
+                        }
+                        else {
+                            req.user = user;
+                            return next();
+                        }
                     }
                 }
+            }
+        }
+        catch (error) {
+            // Handle JWT-related errors here
+            if (error instanceof jsonwebtoken_1.TokenExpiredError) {
+                // Handle token expiration error
+                console.log("Token has expired");
+                return res.status(401).json({ message: "Token has expired" });
+            }
+            else {
+                error.cause = 403;
+                return next(error);
             }
         }
     }));
--- src/middleWare/authentication.ts
+++ src/middleWare/authentication.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import jwt from "jsonwebtoken";
+import jwt, { TokenExpiredError } from "jsonwebtoken"; // Import TokenExpiredError for handling token expiration
 import UserModel from "../../DB/models/user";
 import { asyncHandler } from "../services/errorHandling.js";
 
@@ -8,14 +8,14 @@ export const roles = {
   User: "User",
   Vendor: "Vendor",
 };
+
 export const auth = (accessRoles: string[] = [roles.User]) => {
-  return asyncHandler(
-    async (req: Request, res: Response, next: NextFunction) => {
-      const { authorization } = req.headers;
+  return asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    const { authorization } = req.headers;
+    
+    try {
       if (!authorization?.startsWith(process.env.bearerKey as string)) {
-        const error = new Error("in-valid token or bearer key");
-        (error as any).cause = 200;
-        return next(error);
+        throw new Error("Invalid token or bearer key");
       } else {
         const token = authorization.split(process.env.bearerKey as string)[1];
         const decoded: any = jwt.verify(
@@ -23,20 +23,15 @@ export const auth = (accessRoles: string[] = [roles.User]) => {
           process.env.tokenSignature as string
         );
         if (!decoded?.id || !decoded?.isLoggedIn) {
-          const error = new Error("in-valid token payload");
-          (error as any).cause = 200;
-          return next(error);
+          throw new Error("Invalid token payload");
         } else {
           const user = await UserModel.findById(decoded.id);
+          
           if (!user) {
-            const error = new Error("Not register user");
-            (error as any).cause = 401;
-            return next(error);
+            throw new Error("Not registered user");
           } else {
             if (!accessRoles.includes(user.role)) {
-              const error = new Error("Un-authorized user");
-              (error as any).cause = 403;
-              return next(error);
+              throw new Error("Unauthorized user");
             } else {
               (req as any).user = user;
               return next();
@@ -44,6 +39,16 @@ export const auth = (accessRoles: string[] = [roles.User]) => {
           }
         }
       }
+    } catch (error) {
+      // Handle JWT-related errors here
+      if (error instanceof TokenExpiredError) {
+        // Handle token expiration error
+        console.log("Token has expired");
+        return res.status(401).json({ message: "Token has expired" });
+      } else {
+        (error as any).cause = 403;
+        return next(error);
+      }
     }
-  );
+  });
 };
